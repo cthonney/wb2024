29dc7897aa283c86bfc6e44568e2e9d81d110f5d
Ny Tsiky Razafindrabe <85242561+rovanytsiky@users.noreply.github.com>
2024-05-05 06:08:02 +0400

    contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,33 +1,14 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!
-
   def new
     @contact = Contact.new
   end
-
-  # def create
-  #   success = verify_recaptcha(action: 'login', minimum_score: 0.5, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
-  #   checkbox_success = verify_recaptcha unless success
-  #   if success || checkbox_success
-  #     @contact = Contact.new(contact_params)
-  #     if @contact.save
-  #       redirect_to new_contact_path, notice: "Votre message a été envoyé avec succès."
-  #     else
-  #       render :new
-  #     end
-  #   else
-  #     if !success
-  #       @show_checkbox_recaptcha = true
-  #     end
-  #     render 'new'
-  #   end
-  # end
   def create
+    @contact = Contact.new(contact_params)
     success = verify_recaptcha(action: 'login', minimum_score: 0.5, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
     checkbox_success = verify_recaptcha unless success
-
-    if (success || checkbox_success) && (contact = Contact.new(contact_params)).save
-      redirect_to new_contact_path, notice: "Your message has been successfully sent."
+    if (success || checkbox_success) && @contact.save
+      redirect_to root_path, notice: "Your message has been successfully submitted."
     else
       @show_checkbox_recaptcha = !success
       render :new
@@ -35,9 +16,7 @@ class ContactsController < ApplicationController
   end
 
 
-
   private
-
   def contact_params
     params.require(:contact).permit(:name, :email, :message)
   end
